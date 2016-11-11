f2f46559539eaaf10ead8f6aa861e32cd8cca166
lilymckay <gummybearslily@gmail.com>
2016-11-11 16:11:46 -0500

    Create Codechef.java

---
+++ Codechef.java
@@ -0,0 +1,11 @@
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// 
+		String word="Booooootayyyyyyy";
+		boolean isGirl=false;
+		if(isGirl==true){
+		    System.out.println(word);
+	    for (int x=2;x=5;x+=2);
+	    System.out.println(word);
+		}
+	}
